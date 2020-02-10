fe542084827fbbf33950c9c1b980f01dbdd38b3d
charming-wolf <c.witcher.w@gmail.com>
2020-02-10 15:13:09 +0800

    feat:支持timeout中断请求

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import {
   SDKAdapterInterface,
   StorageType,
   AbstractSDKRequest,
-  formatUrl
+  formatUrl,
+  IRequestMethod,
+  IRequestConfig
 } from '@cloudbase/adapter-interface';
 
 declare const qg;
@@ -78,28 +80,44 @@ function isMatch(): boolean {
 }
 
 class OppoRequest extends AbstractSDKRequest {
+  // 默认不限超时
+  private readonly _timeout: number;
+  // 超时提示文案
+  private readonly _timeoutMsg: string;
+  // 超时受限请求类型，默认所有请求均受限
+  private readonly _restrictedMethods: Array<IRequestMethod>;
+  constructor(config: IRequestConfig = {}) {
+    super();
+    const { timeout, timeoutMsg, restrictedMethods } = config;
+    this._timeout = timeout || 0;
+    this._timeoutMsg = timeoutMsg || '请求超时';
+    this._restrictedMethods = restrictedMethods || ['get', 'post', 'upload', 'download'];
+  }
   public get(options: IRequestOptions): Promise<ResponseObject> {
     return this._request({
       ...options,
       method: 'get'
-    });
+    }, this._restrictedMethods.indexOf('get') !== -1);
   }
   public post(options: IRequestOptions): Promise<ResponseObject> {
     return this._request({
       ...options,
       method: 'post'
-    });
+    }, this._restrictedMethods.indexOf('post') !== -1);
   }
   public download(options: IRequestOptions) {
+    const self = this;
     const {
       url,
       headers
     } = options;
     return new Promise((resolve, reject) => {
-      qg.downloadFile({
+      let timer = null;
+      const task = qg.downloadFile({
         url: formatUrl('https:', url),
         header: headers,
         success(res) {
+          self._clearTimeout(timer);
           if (res.statusCode === 200 && res.tempFilePath) {
             // 由于涉及权限问题，只返回临时链接不保存到设备
             resolve({
@@ -111,20 +129,24 @@ class OppoRequest extends AbstractSDKRequest {
           }
         },
         fail(err) {
+          self._clearTimeout(timer);
           reject(err);
         }
       });
+      timer = self._setTimeout('download',task);
     });
   }
   public upload(options: IUploadRequestOptions): Promise<ResponseObject> {
+    const self = this;
     return new Promise(async resolve => {
+      let timer = null;
       const {
         url,
         file,
         data,
         headers
       } = options;
-      qg.uploadFile({
+      const task = qg.uploadFile({
         url: formatUrl('https:', url),
         filePath: file,
         name: 'file',
@@ -134,6 +156,7 @@ class OppoRequest extends AbstractSDKRequest {
         },
         header: headers,
         success(res) {
+          self._clearTimeout(timer);
           const result = {
             statusCode: res.statusCode,
             data: res.data || {}
@@ -145,12 +168,15 @@ class OppoRequest extends AbstractSDKRequest {
           resolve(result);
         },
         fail(err) {
+          self._clearTimeout(timer);
           resolve(err);
         }
       });
+      timer = self._setTimeout('upload',task);
     });
   }
-  protected _request(options: IRequestOptions): Promise<ResponseObject> {
+  protected _request(options: IRequestOptions, enableAbort: boolean = false): Promise<ResponseObject> {
+    const self = this;
     const method = (String(options.method)).toLowerCase() || 'get';
     return new Promise(resolve => {
       const { url, headers = {}, data, responseType } = options;
@@ -163,9 +189,10 @@ class OppoRequest extends AbstractSDKRequest {
       for (const key in headers) {
         ajax.setRequestHeader(key, headers[key]);
       }
-
+      let timer;
       ajax.onreadystatechange = () => {
         if (ajax.readyState === 4) {
+          self._clearTimeout(timer);
           const result: ResponseObject = {
             statusCode: ajax.status
           };
@@ -173,12 +200,35 @@ class OppoRequest extends AbstractSDKRequest {
             // 上传post请求返回数据格式为xml，此处容错
             result.data = JSON.parse(ajax.responseText);
           } catch (e) { }
-
           resolve(result);
         }
       };
+      if (enableAbort && this._timeout) {
+        timer = setTimeout(() => {
+          console.warn(this._timeoutMsg);
+          ajax.abort();
+        }, this._timeout);
+      }
       ajax.send(method === 'post' && isFormData(data) ? (data as FormData) : JSON.stringify(data || {}));
     });
+  }
+  private _clearTimeout(timer:number|null){
+    if(timer){
+      clearTimeout(timer);
+      timer = null;
+    }
+  }
+  private _setTimeout(method:IRequestMethod,task):number|null{
+    if(!this._timeout || this._restrictedMethods.indexOf(method) === -1){
+      return null;
+    }
+    const timer = setTimeout(() => {
+      console.warn(this._timeoutMsg);
+      try{
+        task.abort();
+      }catch(e){}
+    }, this._timeout);
+    return timer;
   }
 }
 
